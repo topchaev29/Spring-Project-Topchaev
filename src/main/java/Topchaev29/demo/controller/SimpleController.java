a23060b28e4dbc4e6b10034f8ed540be2cb899ec
Alexander Topchaev <topchayev@internet.ru>
2023-12-17 17:51:10 +0500

    Modified the project

--- src/main/java/Topchaev29/demo/controller/SimpleController.java
+++ src/main/java/Topchaev29/demo/controller/SimpleController.java
@@ -1,13 +1,19 @@
 package Topchaev29.demo.controller;
 
 import ch.qos.logback.core.model.Model;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Map;
 
 public class SimpleController {
 
     @GetMapping("/")
-    public String index(Model model) {
-        model.addText("Привет, мы будем говорить об искусственном интеллекте!");
+    public String index(Map<String , Object> model) {
+        model.put("key1", "Искусственный");
+        model.put("key2", "интеллект");
+        model.put("key3", "это");
+        model.put("key4", " - круто!");
         return "index";
     }
 }
